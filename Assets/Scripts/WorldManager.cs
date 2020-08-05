7a7bc424f51192bd471fab24f0c5884cfff4e809
axandros <brumagint@gmail.com>
2020-08-04 20:27:25 -0400

    finished implementing singleton code.  Reset time elapsed on scene load.

--- Assets/Scripts/WorldManager.cs
+++ Assets/Scripts/WorldManager.cs
@@ -7,6 +7,7 @@ public class WorldManager : MonoBehaviour
 {
     static WorldManager _instance = null;
     public static WorldManager Instance { get { return _instance; } }
+    static int _randNumber = 0;
 
     [SerializeField]
     int _lengthOfWorldInSeconds = 120;
@@ -28,10 +29,26 @@ public class WorldManager : MonoBehaviour
 
     private void Awake()
     {
+        int num = (int)( Random.value * 100.0f);
+        //Debug.Log("World Manager Awake: " + this.transform.name + " #" + num);
         if (_instance == null)
         {
+            //Debug.Log(this.transform.name + " setting as Instance.");
+            DontDestroyOnLoad(this.gameObject);
             _instance = this;
-        } else if(_instance != this) { Destroy(this.transform); }
+            _randNumber = num;
+            SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+        } else if(_instance != this) 
+        {
+            //Debug.Log(this.transform.name + " destroying self.");
+            Destroy(this.gameObject); 
+        }
+    }
+
+    private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        //Debug.Log("World Manager LevelLoaded: " + this.transform.name + " #" + _randNumber);
+        _elapsedTime = 0;
     }
 
     // Update is called once per frame
@@ -43,7 +60,7 @@ public class WorldManager : MonoBehaviour
             ResetWorld();
         }
     }
-    private void ResetWorld()
+    public void ResetWorld()
     {
         Scene s = SceneManager.GetSceneByName(_startingWorld);
         if (s.IsValid())
